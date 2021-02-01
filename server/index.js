f2e351e1dfa01e01862689ce3fc2912abf84c02b
Josef Biehler <jbiehler@samhammer.de>
2021-02-01 06:02:54 +0100

    provide data to client via http api

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const cors = require('cors')
+const fs = require("fs");
+const port = 8081
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get("/ids", (req, res) => {
+    const config = getConfig();
+    const ids = fs.readdirSync(`${config.dataDir}/messages`).map(file => {
+        return /(.*)\.json/.exec(file)[1]
+    })
+    res.json(ids);
+})
+
+app.get("/message/:id", (req, res) => {
+    const config = getConfig();
+    const path = `${config.dataDir}/messages/${req.params.id}.json`;
+    if (!fs.existsSync(path)) {
+        res.sendStatus(404);
+        return;
+    }
+
+    const content = JSON.parse(fs.readFileSync(path).toString());
+    res.json(content)
+})
+
+
+app.listen(port, () => console.log("listen on port " + port))
+
+function getConfig() {
+    return JSON.parse(fs.readFileSync("config/config.json").toString())
+}
